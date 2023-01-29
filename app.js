032a9f15f10a7af2bd3430f2cbd9d8b268a87502
hugoagarcia14 <hugoagarcia14@gmail.com>
2023-01-28 21:26:09 -0500

    Se levanta el servidor y se crea la carpeta views

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const publicPath= path.join(__dirname,'public');
+
+const port = 3500;
+
+app.use(express.static(publicPath));
+app.listen(port, ()=>{
+    console.log('server started on http://localhost:3500');
+});
+
+app.get('/',(req, res)=>{
+    res.sendFile(path.join(__dirname,'./views/home.html'));
+});
+app.get('/register',(req, res)=>{
+    res.sendFile(path.join(__dirname,'./views/register.html'));
+});
