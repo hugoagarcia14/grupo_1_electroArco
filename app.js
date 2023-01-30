e8560e760a59e6626f6a57e8bbf10dd3e93a383d
rafael2597 <santopd1997@gmail.com>
2023-01-29 22:35:38 -0500

    Agregando codigo html

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ app.get('/',(req, res)=>{
 app.get('/register',(req, res)=>{
     res.sendFile(path.join(__dirname,'./views/register.html'));
 });
-app.get('/register',(req, res)=>{
-    res.sendFile(path.join(__dirname,'./views/carro_de_compras.html'));
+app.get('/productCart',(req, res)=>{
+    res.sendFile(path.join(__dirname,'./views/productCart.html'));
 });
