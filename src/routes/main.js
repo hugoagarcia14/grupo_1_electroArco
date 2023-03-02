f2b66792b85e206d581bd36de97a278ecbf16a26
hugoagarcia14 <hugoagarcia14@gmail.com>
2023-03-01 21:22:22 -0500

    Se reorganizan las carpetas y se instala multer

--- src/routes/main.js
+++ src/routes/main.js
@@ -4,13 +4,14 @@
 
  const router = express.Router ();
 
- router.get ('/',mainController.index);
+ /*router.get ('/',mainController.index);
  router.get ('/register', mainController.register);
  router.get ('/productdetail', mainController.productdetail);
  router.get ('/productCart', mainController.productCart);
  router.get ('/login', mainController.login);
  router.get ('/formCreationProduct', mainController.formularioCreacionProductos);
  router.get ('/editProduct', mainController.editProduct);
- router.get ('/homeAdmin', mainController.homeAdmin);
+ router.get ('/homeAdmin', mainController.homeAdmin);*/
+ 
  
  module.exports = router;
