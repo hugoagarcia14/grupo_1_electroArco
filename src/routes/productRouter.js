28c9c18b138f6112dbe4d8dd3d2afba3d43e3cdf
hugoagarcia14 <hugoagarcia14@gmail.com>
2023-04-10 21:07:36 -0500

    Se organizan rutas que entren con el login

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -6,9 +6,17 @@ const fs = require('fs');
 
 const multer = require('multer');
 
+const { body } = require('express-validator');
+
 const router = express.Router();
 
 const productController = require('../controllers/productController');
+
+const validations = require('../middlewares/validateRegisterMiddleware');
+
+const guestMiddleware = require('../middlewares/guestMiddleware');
+
+const authMiddleware = require('../middlewares/authMiddlewares');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -22,18 +30,18 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 
-router.get ('/productCart', productController.productCart);
+router.get ('/productCart', authMiddleware, productController.productCart);
 
 router.get ('/productdetail/:id', productController.productdetail);
 
-router.get ('/formCreationProduct', productController.formCreationProduct);
-router.post('/', upload.single('image'), productController.store);
+router.get ('/formCreationProduct', authMiddleware, productController.formCreationProduct);
+router.post('/', upload.single('image'), authMiddleware, productController.store);
 
 router.post('/createProduct', upload.single('image'), productController.store);
 
-router.get ('/editProduct/:id', productController.editProduct);
+router.get ('/editProduct/:id', authMiddleware, productController.editProduct);
 router.put('/editProduct/:id', upload.single('image'), productController.update); 
 
-router.delete('/productdetail/delete/:id', productController.destroy);
+router.delete('/productdetail/delete/:id', authMiddleware, productController.destroy);
 
 module.exports = router;
