11c01ea93e5d5dca4fdb2c4ad07cf6d7375c7404
hugoagarcia14 <hugoagarcia14@gmail.com>
2023-05-02 22:01:20 -0500

    Se organizan los id de la tabla product colors

--- src/database/seeders/20230429043945-ProductsColorsSeeder.js
+++ src/database/seeders/20230429043945-ProductsColorsSeeder.js
@@ -14,23 +14,23 @@ module.exports = {
     */
     await queryInterface.bulkInsert('products_colors', [
       {
-        product_id: 4,
+        product_id: 1,
         color_id:1
       },
       {
-        product_id: 4,
+        product_id: 1,
         color_id:2
       },
       {
-        product_id: 5,
+        product_id: 2,
         color_id:3
       },
       {
-        product_id: 6,
+        product_id: 2,
         color_id:4
       },
       {
-        product_id: 6,
+        product_id: 3,
         color_id:5
       }
     ], {});
